7858133700352070928e985842078661abbba79a
MUNYAKAZIGad <160459884+MUNYAKAZIGad@users.noreply.github.com>
2024-04-16 19:42:31 +0200

    Add files via upload

---
+++ webTechnology/connection.php
@@ -0,0 +1,8 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "bityeartwo2024";
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
---
+++ webTechnology/image.php
@@ -0,0 +1,35 @@
+<?php
+  require 'connection.php';
+  if(isset($_POST["submit"])){  
+    $name = $_POST["name"];
+      if($_FILES["image"]["error"] === 4){
+        echo "<script> alert('Image Does Not Exist');</script>";
+      } else {
+        $fileName = $_FILES['image']['name'];
+        $fileSize = $_FILES['image']['size'];
+        $tmpName = $_FILES['image']['tmp_name'];
+
+        $validImageExtension = ['jpg', 'jpeg', 'png'];
+        $imageExtension = explode('.',$fileName);
+        $imageExtension = strtolower(end($imageExtension));
+        if(!in_array($imageExtension, $validImageExtension)) {
+            echo "<script> alert('Invalid Image Extension');</script>";
+        } else if($fileSize > 1000000){
+            echo "<script> alert('Image Size is Too Large');</script>";
+        } else {
+            $newImageName = uniqid();
+            $newImageName .= '.' . $imageExtension;
+
+            move_uploaded_file($tmpName, 'multimedia/images/'. $newImageName);
+            $query = "INSERT INTO multimedia VALUEs('$name','$newImageName','')";
+            mysqli_query($conn,$query);
+            echo "
+               <script> 
+                   alert('Successfully Uploaded');
+                   document.location.href = 'image.html';
+               </script>
+            ";
+        }
+      }
+  }
+?>
