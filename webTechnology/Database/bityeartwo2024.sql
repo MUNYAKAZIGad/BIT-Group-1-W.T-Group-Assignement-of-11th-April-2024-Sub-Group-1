48998662dae5bcd7b7397b73a1167b3a958b500a
MUNYAKAZIGad <160459884+MUNYAKAZIGad@users.noreply.github.com>
2024-04-16 19:21:05 +0200

    Add files via upload

---
+++ webTechnology/Database/bityeartwo2024.sql
@@ -0,0 +1,336 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 16, 2024 at 07:12 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bityeartwo2024`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article`
+--
+
+CREATE TABLE `article` (
+  `artid` int(11) NOT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `contents` text DEFAULT NULL,
+  `dateofcreation` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `cid` int(11) NOT NULL,
+  `contentid` int(11) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `fid` int(11) NOT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `friendid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `like`
+--
+
+CREATE TABLE `like` (
+  `lid` int(11) NOT NULL,
+  `articleid` int(11) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `multimedia`
+--
+
+CREATE TABLE `multimedia` (
+  `type` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `upload_date` datetime(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `multimedia`
+--
+
+INSERT INTO `multimedia` (`type`, `location`, `upload_date`) VALUES
+('Images', '661eae7d73582.jpg', '0000-00-00 00:00:00.000000'),
+('Video', '661eb1410851b.mp4', '0000-00-00 00:00:00.000000');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `pid` int(11) NOT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `Campus` varchar(100) DEFAULT NULL,
+  `College` varchar(100) DEFAULT NULL,
+  `School` varchar(100) DEFAULT NULL,
+  `Department` varchar(100) DEFAULT NULL,
+  `Level` varchar(50) DEFAULT NULL,
+  `GroupName` varchar(50) DEFAULT NULL,
+  `Regnumber` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `rid` int(11) NOT NULL,
+  `userid` int(11) DEFAULT NULL,
+  `rolename` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `unlike`
+--
+
+CREATE TABLE `unlike` (
+  `lid` int(11) NOT NULL,
+  `contentid` int(11) DEFAULT NULL,
+  `userid` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `creationdate` timestamp NOT NULL DEFAULT current_timestamp(),
+  `activation_code` varchar(50) DEFAULT NULL,
+  `is_activated` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `firstname`, `lastname`, `username`, `email`, `telephone`, `password`, `creationdate`, `activation_code`, `is_activated`) VALUES
+(1, 'MUNYAKAZI', 'Gad', 'mgad', 'mgad@gmail.com', '', '$2y$10$WmaxFex75UaW1K18aubcau1sk3JVpbvKFV7ttRCv59MvaLUtymk6S', '2024-04-11 12:47:19', NULL, 0),
+(3, 'ISHIMWE', 'Jackson', 'ijackson', 'ijackson@gmail.com', '0788854916', '$2y$10$s8X48VNs7NzeboVGUjWP7ufLI9UvFfE5fCaBYOwvgxysmDJ5IMw/y', '2024-04-11 12:51:26', NULL, 0),
+(4, 'ISHIMWE', 'Marveille', 'imarveille', 'imarveille@gmail.com', '0788854916', '$2y$10$Y2jh7tHOz1Sx91Vaa71XzeL3aWtGHgrrkhY.Fd76iEzu7WEJD4dLS', '2024-04-11 12:53:58', NULL, 0),
+(8, 'IMANI', 'Peace', 'ipeace', 'ipeace@gmail.com', '0788854916', '$2y$10$W7JBUHBWiMg3jo4rDPeNl.ev5msirZe3hCyXKjFQGQ8Mnk9GyrZuq', '2024-04-11 12:56:19', NULL, 0),
+(9, 'MUNYAKAZI', 'Peace', 'mpeace', 'mpeace@gmail.com', '0788854916', '$2y$10$hgngEURhqEeDHA3CBwF3Zee11J3p3EJfor1pCsxvkkcRqZiRIDRGq', '2024-04-11 13:49:07', NULL, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `article`
+--
+ALTER TABLE `article`
+  ADD PRIMARY KEY (`artid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`cid`),
+  ADD KEY `contentid` (`contentid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `friends`
+--
+ALTER TABLE `friends`
+  ADD PRIMARY KEY (`fid`),
+  ADD KEY `userid` (`userid`),
+  ADD KEY `friendid` (`friendid`);
+
+--
+-- Indexes for table `like`
+--
+ALTER TABLE `like`
+  ADD PRIMARY KEY (`lid`),
+  ADD KEY `articleid` (`articleid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`pid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`rid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `unlike`
+--
+ALTER TABLE `unlike`
+  ADD PRIMARY KEY (`lid`),
+  ADD KEY `contentid` (`contentid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `article`
+--
+ALTER TABLE `article`
+  MODIFY `artid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `cid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `friends`
+--
+ALTER TABLE `friends`
+  MODIFY `fid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `like`
+--
+ALTER TABLE `like`
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `pid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `unlike`
+--
+ALTER TABLE `unlike`
+  MODIFY `lid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `article`
+--
+ALTER TABLE `article`
+  ADD CONSTRAINT `article_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`contentid`) REFERENCES `article` (`artid`),
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `friends`
+--
+ALTER TABLE `friends`
+  ADD CONSTRAINT `friends_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `friends_ibfk_2` FOREIGN KEY (`friendid`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `like`
+--
+ALTER TABLE `like`
+  ADD CONSTRAINT `like_ibfk_1` FOREIGN KEY (`articleid`) REFERENCES `article` (`artid`),
+  ADD CONSTRAINT `like_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `profile_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `role`
+--
+ALTER TABLE `role`
+  ADD CONSTRAINT `role_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`id`);
+
+--
+-- Constraints for table `unlike`
+--
+ALTER TABLE `unlike`
+  ADD CONSTRAINT `unlike_ibfk_1` FOREIGN KEY (`contentid`) REFERENCES `article` (`artid`),
+  ADD CONSTRAINT `unlike_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
