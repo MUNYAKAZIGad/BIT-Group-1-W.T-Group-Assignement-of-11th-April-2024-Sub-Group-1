6fdd5a9102f366c8b4cda4efe4198cd2c0d452c7
MUNYAKAZIGad <160459884+MUNYAKAZIGad@users.noreply.github.com>
2024-04-16 19:26:27 +0200

    Delete webTechnology directory

--- webTechnology/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "bityeartwo2024";
-$conn = new mysqli($servername, $username, $password, $dbname);
-?>
